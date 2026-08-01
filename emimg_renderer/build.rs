fba1243f5581897f9bcb46d5087dd861d2f44925
Josh Megnauth <jo.sh@tutanota.com>
2026-07-31 21:39:13 -0400

    Add a rust-gpu shader builder build.rs
    The `build.rs` is valid for nightly. I tested it on a WGPU project which
    also uses rust-gpu for shaders. The code differs from the repository
    because the shape of the API changed since the latest release on
    crates.io.
    
    Additionally, I fixed up CI a bit by adding the dependencies needed to
    build image-rs. It will probably still fail because reasons.

--- emimg_renderer/build.rs
+++ emimg_renderer/build.rs
@@ -1,4 +1,46 @@
+use std::{fs, path::Path};
+
+use spirv_builder::{ShaderPanicStrategy, SpirvBuilder};
+
+fn build_spirv(path: impl AsRef<Path>, name: &str) {
+    let metadata = SpirvBuilder::new(path, "spirv-unknown-vulkan1.3")
+        .shader_panic_strategy(ShaderPanicStrategy::DebugPrintfThenExit {
+            print_inputs: true,
+            print_backtrace: true,
+        })
+        .build()
+        .unwrap_or_else(|e| panic!("Failed building shader: {name}\nError: {e}"));
+    println!(
+        "cargo::rustc-env={}={}",
+        name,
+        metadata
+            .module
+            .unwrap_single()
+            .to_str()
+            .expect("Final path should be UTF-8")
+    );
+}
+
 fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
+    let shaders = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .expect("Build with Cargo to ensure path env vars exist")
+        .join("rust_shaders");
+    println!("cargo::rerun-if-changed={}", shaders.to_str().unwrap());
+
+    for shader in fs::read_dir(shaders).expect("shaders directory should be in root") {
+        let shader = shader.expect("should be able to read/access shader directory");
+        assert!(
+            shader.file_type().unwrap().is_dir(),
+            "shaders directory should only have directories"
+        );
+        let path = shader.path();
+        let name = shader.file_name().to_ascii_uppercase();
+        let name = name.to_str().unwrap();
+        build_spirv(path, name);
+    }
+
     #[cfg(feature = "no-libc")]
     println!("cargo::rustc-link-arg=-nostartfiles")
 }
